0d5dffd3d29d573d6271111bb05fe9501e50459b
Rowen <129540940+rowen-dev@users.noreply.github.com>
2024-10-18 21:17:55 +0400

    Add files via upload

---
+++ libra.mus-main/src/config.roman.js
@@ -0,0 +1,12 @@
+module.exports = {
+    github: "rowen-dev",
+    instagram: "rowen.wrest",
+    discord: {
+        username: "'Rowen",
+        id: "976196074157015110",
+        avatarURL: "https://avatars.githubusercontent.com/u/129540940?s=96&v=4"
+    },
+    website: {
+        description: "Front-end developer. My name is Revan, I live in Azerbaijan and I am a high school student."
+    }
+}
